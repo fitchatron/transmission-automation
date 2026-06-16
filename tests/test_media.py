4afda3f8f0cbae9e7bdb84ce0e473691c5fd31e5
fitchatron <53107122+fitchatron@users.noreply.github.com>
2026-06-16 11:11:12 +1000

    add tests

--- tests/test_media.py
+++ tests/test_media.py
@@ -1,6 +1,11 @@
 import pytest
 from utils.media import is_string_match
 
+"""
+first == torrent_name
+second == downloaded media dir / filename
+"""
+
 
 @pytest.mark.parametrize(
     "first, second, expected_match, threshold",
@@ -9,20 +14,18 @@ from utils.media import is_string_match
         ("The Matrix", "Interstellar", False, 90.0),
         ("The Matrix", "The Matrx", True, 80.0),
         ("The Matrix", "the.matrx", True, 90.0),
-        ("Rick.and.Morty.S09E02.1080p.x265-ELiTE", "Rick and Morty S09E02 1080p x265-ELiTE EZTV", True, 90.0),
-        # (
-        #     "Rick and Morty S09E03 Rick Fu Hustle 1080p AMZN WEB-DL DDP5 1 H 264-playWEB[EZTVx.to].mkv",
-        #     "Rick and Morty S09E03 Rick Fu Hustle 1080p AMZN WEB-DL DDP5 1 H 264-playWEB EZTV",
-        #     True,
-        #     None,
-        # ),
-        # ("www.UIndex.org    -    Euphoria US S03E07 1080p x265-ELiTE", "", True, None),
-        # (
-        #     "www.UIndex.org    -    Euphoria US S03E08 In God We Trust 1080p AMZN WEB-DL DDP5 1 H 264-NT",
-        #     "",
-        #     True,
-        #     None,
-        # ),
+        (
+            "Rick.and.Morty.S09E02.1080p.x265-ELiTE",
+            "Rick and Morty S09E02 1080p x265-ELiTE EZTV",
+            True,
+            90.0,
+        ),
+        (
+            "Rick.and.Morty.S09E04.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
+            "Rick.and.Morty.S09E04.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
+            True,
+            90.0,
+        ),
     ],
 )
 def test_is_string_match(first, second, expected_match, threshold):
--- tests/test_transmission.py
+++ tests/test_transmission.py
@@ -4,10 +4,12 @@ import subprocess
 from utils.transmission import list_torrents
 
 
+#      7    90%   689.1 MB  19 sec       0.0  2960.0    0.0  Up & Down    Rick.and.Morty.S09E04.1080p.WEB.h264-EDITH[EZTVx.to].mkv
 def test_list_torrents_parses_name_with_multiple_spaces(monkeypatch):
     stdout = "\n".join(
         [
             "    ID   Done       Have  ETA           Up    Down  Ratio  Status       Name",
+            "     7     0%       None  Unknown      0.0     0.0   None  Up & Down    Rick.and.Morty.S09E04.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
             "    10   100%    4.53 GB  Done        41.0     0.0    0.0  Seeding      www.UIndex.org    -    The.Boys.S05E01.1080p.WEB.h264-ETHEL",
             "    11   100%   640.0 MB  Done         0.0     0.0    0.0  Idle         The.Boys.S05E02.1080p.HEVC.x265-MeGusta[EZTVx.to].mkv",
             "    12   100%    3.60 GB  Done         0.0     0.0    0.0  Seeding      Saturday.Night.Live.S51E17.Colman.Domingo.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
@@ -16,22 +18,28 @@ def test_list_torrents_parses_name_with_multiple_spaces(monkeypatch):
     )
 
     def mock_run(*_args, **_kwargs):
-        return subprocess.CompletedProcess(args=[], returncode=0, stdout=stdout, stderr="")
+        return subprocess.CompletedProcess(
+            args=[], returncode=0, stdout=stdout, stderr=""
+        )
 
     monkeypatch.setattr(subprocess, "run", mock_run)
 
     torrents = list_torrents(logging.getLogger(__name__))
 
-    assert len(torrents) == 3
-    assert torrents[0]["id"] == "10"
-    assert torrents[0]["status"] == "Seeding"
-    assert torrents[0]["name"] == "www.UIndex.org    -    The.Boys.S05E01.1080p.WEB.h264-ETHEL"
+    assert len(torrents) == 4
+    assert torrents[1]["id"] == "10"
+    assert torrents[1]["status"] == "Seeding"
+    assert (
+        torrents[1]["name"]
+        == "www.UIndex.org    -    The.Boys.S05E01.1080p.WEB.h264-ETHEL"
+    )
 
 
 def test_list_torrents_parses_status_with_spaces(monkeypatch):
     stdout = "\n".join(
         [
             "    ID   Done       Have  ETA           Up    Down  Ratio  Status       Name",
+            "     7    90%   689.1 MB  19 sec       0.0  2960.0    0.0  Up & Down    Rick.and.Morty.S09E04.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
             "    10     0%   16.46 MB  10 hrs       0.0   162.0    0.0  Downloading  www.UIndex.org    -    The.Boys.S05E01.1080p.WEB.h264-ETHEL",
             "    11     2%   15.80 MB  5 min        4.0  2066.0    0.0  Up & Down    The.Boys.S05E02.1080p.HEVC.x265-MeGusta[EZTVx.to].mkv",
             "    12     0%       None  Unknown      0.0     0.0   None  Idle         Saturday.Night.Live.S51E17.Colman.Domingo.1080p.WEB.h264-EDITH[EZTVx.to].mkv",
@@ -40,13 +48,17 @@ def test_list_torrents_parses_status_with_spaces(monkeypatch):
     )
 
     def mock_run(*_args, **_kwargs):
-        return subprocess.CompletedProcess(args=[], returncode=0, stdout=stdout, stderr="")
+        return subprocess.CompletedProcess(
+            args=[], returncode=0, stdout=stdout, stderr=""
+        )
 
     monkeypatch.setattr(subprocess, "run", mock_run)
 
     torrents = list_torrents(logging.getLogger(__name__))
 
-    assert len(torrents) == 3
-    assert torrents[1]["id"] == "11"
-    assert torrents[1]["status"] == "Up & Down"
-    assert torrents[1]["name"] == "The.Boys.S05E02.1080p.HEVC.x265-MeGusta[EZTVx.to].mkv"
+    assert len(torrents) == 4
+    assert torrents[2]["id"] == "11"
+    assert torrents[2]["status"] == "Up & Down"
+    assert (
+        torrents[2]["name"] == "The.Boys.S05E02.1080p.HEVC.x265-MeGusta[EZTVx.to].mkv"
+    )
